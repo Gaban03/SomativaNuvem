a0d3ffe826b4f25cc40741924b547c586c75bdd5
Murilo-Herrick <muriloherrick@gmail.com>
2025-05-26 22:19:57 -0300

    anat - Pronto

--- Project_SkyTechAeroSolutions/anat_mock/app.py
+++ Project_SkyTechAeroSolutions/anat_mock/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, render_template
 import mysql.connector
 from flask_cors import CORS
 
@@ -8,25 +8,27 @@ CORS(app)  # Permite CORS para todas as rotas e origens
 db_config = {
     'host': 'db',
     'user': 'root',
-    'password': 'password',
-    'database': 'medical_clinic_db'
+    'password': '1234',
+    'database': 'aero_solutions'
 }
 
-@app.route('/planos', methods=['GET'])
-def listar_planos():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/anat_mock', methods=['GET'])
+def listar():
     try:
         conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
 
-        cursor.execute("SELECT id_plano_saude, nome FROM plano_saude")
+        cursor.execute('''SELECT m.id_mecanico, m.nome, m.cpf, c.apto FROM mecanico m LEFT JOIN certificacao c ON m.id_mecanico = c.id_mecanico;''')
         dados = cursor.fetchall()
 
         cursor.close()
         conn.close()
 
-        planos = [{"id": row[0], "nome": row[1]} for row in dados]
-
-        return jsonify(planos)
+        return jsonify(dados)
     except Exception as e:
         return jsonify({"message": str(e)}), 500
 
---
+++ Project_SkyTechAeroSolutions/anat_mock/static/script.js
@@ -0,0 +1,24 @@
+// Aguar o carregamento completo da pagina antes de rodar o js
+document.addEventListener('DOMContentLoaded', function () {
+    const tabelaBody = document.querySelector('#tableMecanicosCertificacoes tbody');
+
+    function atualizarTabela() {
+        fetch('/anat_mock')
+        .then(response => response.json())
+        .then(mecanicos_certificacao => {
+         // Limpa a tabela
+        tabelaBody.innerHTML = '';
+        
+        mecanicos_certificacao.forEach(mecanicos_certificacao => {
+        const row = tabelaBody.insertRow();
+        row.insertCell(0).textContent = mecanicos_certificacao[0];
+        row.insertCell(1).textContent = mecanicos_certificacao[1];
+        row.insertCell(2).textContent = mecanicos_certificacao[2];
+        row.insertCell(3).textContent = mecanicos_certificacao[3] === 1 ? 'Sim' : (mecanicos_certificacao[3] === 0 ? 'Não' : 'Sem certificação');
+         });
+        })
+        .catch(error => console.error('Erro ao carregar certificações:', error));
+    }
+        
+    atualizarTabela();
+}) ;
